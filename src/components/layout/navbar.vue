deae83ce41d81ff6a96bb8761bdad91d7857d9dd
Oswaldo Freitas <oswaldo.milet@gmail.com>
2017-06-06 09:06:20 -0300

    add external links (github, twitter, slack)

--- src/components/layout/navbar.vue
+++ src/components/layout/navbar.vue
@@ -4,8 +4,9 @@
     show-shadow,
     brand-logo='/static/logo.png',
     @menu-clicked="$emit('menu-clicked')")
-    vulma-navitem(slot='right', icon='fa-github', :is-tab='false').is-hidden-mobile
-    vulma-navitem(slot='right', icon='fa-twitter', :is-tab='false').is-hidden-mobile
+    vulma-navitem(slot='right', icon='fa-github', :is-tab='false', href='https://github.com/oswaldofreitas/vulma', open-new-tab='true').is-hidden-mobile
+    vulma-navitem(slot='right', icon='fa-twitter', :is-tab='false', href='https://twitter.com/VulmaUI', open-new-tab='true').is-hidden-mobile
+    vulma-navitem(slot='right', icon='fa-slack', :is-tab='false', href='https://vulma.slack.com', open-new-tab='true').is-hidden-mobile
 </template>
 
 <script>
