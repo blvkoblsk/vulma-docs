9523e6dd3c7e6e143ec6415a71aebe4e72942320
Oswaldo Freitas <oswaldo.milet@gmail.com>
2017-06-30 18:35:58 -0300

    add radio size prop doc

--- src/docs/radio.vue
+++ src/docs/radio.vue
@@ -15,7 +15,8 @@ export default {
             { name: 'label', type: 'String', default: '', description: this.$t('field_label') },
             { name: 'src', type: 'Array', default: '', description: this.$t('source_array') },
             { name: 'text', type: 'String', default: '', description: this.$t('text_property') },
-            { name: 'value', type: 'String', default: '', description: this.$t('value_property') }
+            { name: 'value', type: 'String', default: '', description: this.$t('value_property') },
+            { name: 'size', type: 'String', default: '', description: this.$t('bulma_size') }
           ]
         }
       ]
